20421a196636d76f133ef5e6a8f994516f5450a2
yuvraj singh jadon <yuvrajjadon10@gmail.com>
2023-04-03 01:27:37 +0530

    initial commit

---
+++ src/components/Who.jsx
@@ -0,0 +1,114 @@
+import React, { Suspense } from "react";
+import styled from "styled-components";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Cube from "./Cube";
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 60px;
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+
+  @media only screen and (max-width: 768px) {
+    align-items: center;
+    text-align: center;
+  }
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 120px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Who = () => {
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <Canvas camera={{ position: [5, 5, 5], fov: 25 }}>
+            <Suspense fallback={null}>
+              <ambientLight intensity={0.5} />
+              <directionalLight position={[3, 2, 1]} />
+              <Cube />
+              <OrbitControls enableZoom={false} autoRotate />
+            </Suspense>
+          </Canvas>
+        </Left>
+        <Right>
+          <Title className="text-yellow-600">
+            Think outside the square space
+          </Title>
+          <WhatWeDo>
+            <Subtitle>Who AM I</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            Hello, I'm Yuvraj Singh Jadon, a 3rd-year B.Tech IT student at the
+            National Institute of Technology Karnataka. I'm a budding web
+            developer with a keen interest in creating visually appealing and
+            user-friendly websites. As a student of computer science, I am
+            always eager to learn new things and experiment with new
+            technologies. When I'm not coding, you can find me playing sports,
+            learning new skills, or exploring the outdoors.
+          </Desc>
+          <Button>MY work</Button>
+        </Right>
+      </Container>
+    </Section>
+  );
+};
+
+export default Who;
